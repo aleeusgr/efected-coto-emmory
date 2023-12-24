41cd3b0fbe6cd1808e398dac307a2e161c112a6f
Alex <alexeusgr@gmail.com>
2023-12-24 11:08:12 +0200

    nix flake init

--- flake.nix
+++ flake.nix
@@ -1,27 +1,21 @@
 {
-  description = "A flake for building a Rust workspace using buildRustPackage.";
-
   inputs = {
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.follows = "rust-overlay/flake-utils";
-    nixpkgs.follows = "rust-overlay/nixpkgs";
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = inputs: with inputs;
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        code = pkgs.callPackage ./. { inherit nixpkgs system rust-overlay; };
-      in rec {
-        packages = {
-          app = code.app;
-          wasm = code.wasm;
-          all = pkgs.symlinkJoin {
-            name = "all";
-            paths = with code; [ app wasm ];
-          };
-        default = packages.all;
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
-      }
-    );
+      });
 }
