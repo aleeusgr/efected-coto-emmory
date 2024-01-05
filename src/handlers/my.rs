8559281eccc962c198f7cd4232211be60f8b0338
Alex <alexeusgr@gmail.com>
2024-01-05 17:49:20 +0200

    encode image

--- src/handlers/my.rs
+++ src/handlers/my.rs
@@ -3,6 +3,9 @@ use axum::{
     response::{Html, IntoResponse},
     http::{StatusCode, Uri, header::{self, HeaderMap, HeaderName}},
 };
+use std::path::PathBuf;
+use std::io::{BufWriter, Cursor};
+use image::ImageFormat;
 
 // this is a handler, a function that is used in a Router
 // A handler is an async function that accepts zero or more “extractors” as arguments and returns something that can be converted into a response.
@@ -16,12 +19,14 @@ pub async fn html() -> Html<&'static str> {
 }
 // stream.
 pub async fn get_image() -> impl axum::response::IntoResponse {
-    //let img_path = PathBuf::from("<path/to/image>").join("ferris.png");
-    //let image = image::io::Reader::open(&img_path).unwrap().decode().unwrap();
-
+    let img_path = PathBuf::from("assets/").join("a_logo.png");
+    let image = image::io::Reader::open(&img_path).unwrap().decode().unwrap();
+    let mut buffer = BufWriter::new(Cursor::new(Vec::new()));
+    image.write_to(&mut buffer, ImageFormat::Png).unwrap();
+    let bytes: Vec<u8> = buffer.into_inner().unwrap().into_inner();
     (
         axum::response::AppendHeaders([(header::CONTENT_TYPE, "image/png")]),
-        "Hello!!!".to_string()
+        bytes
         // image.into_bytes()
     )
 }
